b4f6bc3c4445594153267c4f9621e6dd88c30110
gonzaloiv <gonzalo.iglesias@teimas.com>
2016-10-15 13:33:32 +0200

    métodos de usuario: status + collect, botones interfaz

--- app/controller.py
+++ app/controller.py
@@ -1,70 +1,44 @@
 import telebot
+from telebot import types
 from peewee import *
 
 from config import API_KEY
 
 from helper import *
 from models import User
-from telebot import types
-import datetime
 
 def initialize_bot():
 
 	bot = telebot.TeleBot(API_KEY)
 
-
 	@bot.message_handler(commands=['help'])
 	def send_welcome(message):
 		bot.send_message(message.chat.id, "Futbol Manager  \n Registrar Jugador - /register")
 
-		
 	@bot.message_handler(commands=['start'])
 	def send_start(message):
 		try:
-			User(name=message.from_user.first_name, telegram_id=message.from_user.id, level=0, experience=0, money=0, fans=1).save()
-			bot.reply_to(message, "Usuario registrado:  " + User.get(User.telegram_id == message.from_user.id).name)
-			#bot.reply_to(message,"hola")
+			user = User(name=message.from_user.first_name, telegram_id=message.from_user.id, level=0, experience=0, money=0, fans=1)
+			user.save()
+			bot.send_message(message.chat_id, "Usuario registrado:  " + user.name)
 		except:
-			bot.reply_to(message, "Usuario ya registrado: " + User.get(User.telegram_id == message.from_user.id).name 
-			+ "Estado del usuario: /status")
+			bot.reply_to(message, "Usuario ya registrado:  " + user.name)
 
 	@bot.message_handler(commands=['status'])
 	def send_status(message):
 		try:
-			bot.send_message(message.chat.id, u"\U0001F464" + " Nombre: " + User.get(User.telegram_id == message.from_user.id).name + "\n" 
-			+ "Nivel: " + str(User.get(User.telegram_id == message.from_user.id).level) + "\n" 
-			+ "Experiencia: " + str(User.get(User.telegram_id == message.from_user.id).experience) + "\n" 
-			+ "Dinero: " + str(User.get(User.telegram_id == message.from_user.id).money) + "\n"
-			+ "Socios: " + str(User.get(User.telegram_id == message.from_user.id).fans) + "\n"
-			+ "Rec: " + str(User.get(User.telegram_id == message.from_user.id).lastRecolect)
-			)
-
-			#bot.send_message(message.chat.id, "Nombre: " + User.get(User.telegram_id == message.from_user.id).name)
+			user = User.get(User.telegram_id == message.from_user.id)
+			bot.reply_to(message, user.status())
 		except Exception as e:
 			bot.reply_to(message, cowsay(e.message))
 
-
-
 	@bot.message_handler(commands=['collect'])
-	def send_status(message):
+	def send_status_collect(message):
 		try:
-			if(User.get(User.telegram_id == message.from_user.id).lastRecolect < datetime.datetime.now() - datetime.timedelta(minutes=1)):
-				User.update(money = User.money + User.fans * 1).where(User.telegram_id == message.from_user.id).execute()
-				User.update(lastRecolect = datetime.datetime.now()).where(User.telegram_id == message.from_user.id).execute()
-				#User.update(fans = 2).where(User.telegram_id == message.from_user.id).execute()
-				bot.send_message(message.chat.id, u"\U0001F464" + " Nombre: " + User.get(User.telegram_id == message.from_user.id).name + "\n" 
-				+ "Nivel: " + str(User.get(User.telegram_id == message.from_user.id).level) + "\n" 
-				+ "Experiencia: " + str(User.get(User.telegram_id == message.from_user.id).experience) + "\n" 
-				+ "Dinero: " + str(User.get(User.telegram_id == message.from_user.id).money) + "\n"
-				+ "Socios: " + str(User.get(User.telegram_id == message.from_user.id).fans) + "\n"
-				+ "Rec: " + str(User.get(User.telegram_id == message.from_user.id).lastRecolect)
-				)
-			else:
-				bot.send_message(message.chat.id, "Debes esperar 1 min desde la ultima vez para recolectar")	
-
-			#bot.send_message(message.chat.id, "Nombre: " + User.get(User.telegram_id == message.from_user.id).name)
+			user = User.get(User.telegram_id == message.from_user.id)
+			bot.reply_to(message, user.collect())
 		except Exception as e:
-			bot.reply_to(message, cowsay(e.message))			
+			bot.reply_to(message, cowsay(e.message))
 
 	@bot.message_handler(commands=['foto'])
 	def send_photo(message):
@@ -77,6 +51,18 @@ def initialize_bot():
 
 	@bot.message_handler(func=lambda m: True)
 	def echo_all(message):
-		bot.reply_to(message, message.text + message.from_user.first_name)
+		markup = types.ReplyKeyboardMarkup()
+		markup.add('Collect', 'Status')
+		bot.register_next_step_handler(msg, process_keyboard_input)
+	def process_keyboard_input(message):
+		user = User.get(User.telegram_id == message.from_user.id)
+		try:
+			if(message.text == u'Collect'):
+				bot.reply_to(message, user.collect())
+			if(message.text == u'Status'):
+				bot.reply_to(message, user.status())
+		except Exception as e:
+			bot.reply_to(message, cowsay(e.message))
+
 
 	return bot
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+import telebot
 from peewee import *
+import datetime
 
 from config import DATABASE_NAME
 
@@ -20,6 +22,25 @@ class User(BaseModel):
   fans = IntegerField()
   lastRecolect = TimestampField()
   joinDate = TimestampField()
+
+  def collect(self):
+    if(self.lastRecolect < datetime.datetime.now() - datetime.timedelta(minutes=1)):
+      self.update(money = self.money + self.fans * 1).where(self.telegram_id == message.from_user.id).execute()
+      self.update(lastRecolect = datetime.datetime.now()).where(self.telegram_id == message.from_user.id).execute()
+      #self.update(fans = 2).where(self.telegram_id == message.from_user.id).execute()
+      return "Recolectado!"
+    else:
+      return "Debes esperar 1 min desde la ultima vez para recolectar"
+
+  def status(self):
+      return (
+        u"\U0001F464" + " Nombre: " + self.name + "\n"
+        + "Nivel: " + str(self.level) + "\n"
+        + "Experiencia: " + str(self.experience) + "\n"
+        + "Dinero: " + str(self.money) + "\n"
+        + "Socios: " + str(self.fans) + "\n"
+        + "Rec: " + str(self.lastRecolect)
+      )
 
 db.drop_tables([User])
 db.create_tables([User])
